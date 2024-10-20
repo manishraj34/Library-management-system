faeb055008721f912530a11e2c0f5e7c4751b36d
manishraj34 <mrpmanish1@gmail.com>
2024-10-20 20:49:02 +0530

    Adding no 2

--- models.py
+++ models.py
@@ -3,3 +3,28 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
+
+class Book(Base):
+    __tablename__ = "books"
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    author = Column(String, nullable=False)
+    isbn = Column(String, nullable=False)
+    count = Column(Integer, nullable=False, default = 1)
+
+class Member(Base):
+    __tablename__ = 'members'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+
+class Transaction(Base):
+    __tablename__ = 'transactions'
+    id = Column(Integer, primary_key=True)
+    book_id = Column(Integer, ForeignKey('books.id'), nullable=False)
+    member_id = Column(Integer, ForeignKey('members.id'), nullable=False)
+    issue_date = Column(Date, nullable=False)
+    return_date = Column(Date, nullable=True)
+
+    book = relationship("Book")
+    member = relationship("Member")
